71be765671b8b20252db9764ecfde4c62e9f2a08
TropicalShadow <ddlogger123@gmail.com>
2023-05-17 16:13:18 +0100

    ToDo - info

--- main.py
+++ main.py
@@ -16,14 +16,24 @@ class Main:
     async def start(self):
         self.database = DatabaseManager()
         await self.database.check_db()
-        
-        if(self.development):
+
+        if self.development:
             await self.run_development()
         else:
             await self.run_production()
-    
-    async def run_production(self):...
-        #TODO - Implement production code
+
+    async def run_production(self):
+        ...
+
+    # TODO - Implement production code
+
+    ## TODO - Process
+    #   - 1. Get all ips from database
+    #   - 2. Scan all ips
+    #   - 3. Get all services from ips
+    #   - 4. Get all products from services
+    #   - 5. Save all products to database
+    #   - 6. Repeat every 24 hours
 
     async def run_development(self, test_ip: str = "127.0.0.1"):
         data = NmapScanner.scan(test_ip)
